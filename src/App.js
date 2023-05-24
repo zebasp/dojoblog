2ac264c77b9967681d15e9a16d9e44b7932a933e
Sebastian Paz <pazseba@hotmail.com>
2023-05-24 17:57:52 -0300

    5 - multiple components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar"
+import Home from "./Home";
 
 function App() {
-  const title = 'Welcome to the new blog'
-  const likes = 50
-  const link = "http://www.google.com"
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{'hello ninjas'}</p>
-        <p>[1,2,3,4,5,8]</p>
-        <p>{Math.random()}</p>
-        <a href={link} >Google site</a>
+        <Home />
       </div>
     </div>
   );
