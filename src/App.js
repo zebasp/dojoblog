402904ed4e21fd51ea1ce88b97022851ab86b274
Sebastian Paz <pazseba@hotmail.com>
2023-05-24 16:49:30 -0300

    4 - Dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 
 function App() {
+  const title = 'Welcome to the new blog'
+  const likes = 50
+  const link = "http://www.google.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{'hello ninjas'}</p>
+        <p>[1,2,3,4,5,8]</p>
+        <p>{Math.random()}</p>
+        <a href={link} >Google site</a>
       </div>
     </div>
   );
