0be72a6ed12c918f24db1bedee7f21e4e06e36de
Sebastian Paz <pazseba@hotmail.com>
2023-05-26 11:25:53 -0300

    14 - useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,11 @@ const handleDelete = (id) => {
   const newBlogs = blogs.filter(blog => blog.id !== id)
   setBlogs(newBlogs)
 }
+
+useEffect(() => {
+  console.log('useEffect ran')
+  console.log(blogs)
+})
 
   return (
     <div className="home">
