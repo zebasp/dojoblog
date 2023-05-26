eac6b24794754871f60e3efc4638e595ca3112b2
Sebastian Paz <pazseba@hotmail.com>
2023-05-26 11:21:27 -0300

    13 - functions as props

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,14 @@ const [blogs, setBlogs] = useState([
     {title: 'web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
 ]);
 
+const handleDelete = (id) => {
+  const newBlogs = blogs.filter(blog => blog.id !== id)
+  setBlogs(newBlogs)
+}
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs"/>
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
     </div>
   );
 };
