a1f04f6654dfcac081887479b0e9b9a69b409fd3
Sebastian Paz <pazseba@hotmail.com>
2023-05-26 12:46:14 -0300

    17 - fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -3,28 +3,21 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-const [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-    {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    {title: 'web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-]);
-
-const [name, setName] = useState('mario');
-
-const handleDelete = (id) => {
-  const newBlogs = blogs.filter(blog => blog.id !== id)
-  setBlogs(newBlogs)
-}
+const [blogs, setBlogs] = useState(null);
 
 useEffect(() => {
-  console.log('useEffect ran')
-  console.log(name)
-}, [name])
+  fetch('http://localhost:8000/blogs')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+      setBlogs(data)
+    })
+}, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
-      <button onClick={() => setName('Luigi')}>Change name</button>
+      {blogs && <BlogList blogs={blogs} title="All blogs"/>}
     </div>
   );
 };
