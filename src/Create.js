ccb7168744e3efdca12f9a5c37544716aac59fa3
Sebastian Paz <pazseba@hotmail.com>
2023-05-27 15:54:52 -0300

    29 - making a Post request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,21 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+    setIsPending(true)
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    })
+      .then( () => {
+          console.log('new blog added')
+          setIsPending(false)
+      })
   };
 
   return (
@@ -33,7 +43,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
