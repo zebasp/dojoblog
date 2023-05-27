1af0d4e3a870043b193895084d7d16002a48fc88
Sebastian Paz <pazseba@hotmail.com>
2023-05-26 22:08:31 -0300

    25 - Route parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Create from "./Create"
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
             <Route path="/" element={<Home />}>
             </Route>
             <Route path="/create" element={<Create />}>
+            </Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}>
             </Route>
           </Routes>
         </div>
