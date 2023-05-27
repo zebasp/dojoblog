5e39bec5618d2ef4fbb6b4ac51df01ae1b4a5da0
Sebastian Paz <pazseba@hotmail.com>
2023-05-27 16:08:01 -0300

    30 - programmatic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    setIsPending(true)
-    fetch('http://localhost:8000/blogs', {
-      method: 'POST',
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(blog)
-    })
-      .then( () => {
-          console.log('new blog added')
-          setIsPending(false)
-      })
+    setIsPending(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+      navigate("/");
+    });
   };
 
   return (
