0fffdf5af2e3d56a7322085cdd55c180ee5c8889
Sebastian Paz <pazseba@hotmail.com>
2023-05-27 16:22:47 -0300

    32 - 404 pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Create from "./Create"
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
             <Route path="/create" element={<Create />}>
             </Route>
             <Route path="/blogs/:id" element={<BlogDetails />}>
+            </Route>
+            <Route path="*" element={<NotFound />}>
             </Route>
           </Routes>
         </div>
